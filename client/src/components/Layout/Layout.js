ee278f474cf4856803a45280e52d5328d3aa842b
Randy Shao <17rs63@queensu.ca>
2021-01-31 12:32:29 -0500

    Added navigation

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import styles from './Layout.module.css';
+import Navbar from '../Navbar/Navbar';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
+      <Navbar />
       {children}
       <footer className={styles.footer}>Powered by Randy Shao</footer>
     </div>
