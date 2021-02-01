17812d1a420c2aff99d1bb2ba74ab482027916b9
Randy Shao <17rs63@queensu.ca>
2021-02-01 01:53:41 -0500

    hourly events data chart

--- client/src/pages/daily-events.js
+++ client/src/pages/daily-events.js
@@ -1,21 +1,8 @@
 import Head from 'next/head';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout/Layout';
 import StackedChart from '../components/Chart/StackedChart';
-// import useResizeObserver from '../hooks/useResizeObserver';
-// import {
-//   select,
-//   axisBottom,
-//   stack,
-//   max,
-//   scaleLinear,
-//   axisLeft,
-//   stackOrderAscending,
-//   area,
-//   scalePoint,
-//   curveCardinal,
-// } from 'd3';
 
 const DailyEvents = ({ stats }) => {
   const allKeys = ['events'];
@@ -25,53 +12,6 @@ const DailyEvents = ({ stats }) => {
   };
 
   const [keys, setKeys] = useState(allKeys);
-  // const [data, setData] = useState(stats);
-
-  // const svgRef = useRef();
-  // const wrapperRef = useRef();
-  // const dimensions = useResizeObserver(wrapperRef);
-
-  // useEffect(() => {
-  //   const svg = select(svgRef.current);
-  //   const { width, height } =
-  //     dimensions || wrapperRef.current.getBoundingClientRect();
-
-  //   const stackGenerator = stack().keys(keys).order(stackOrderAscending);
-  //   const layers = stackGenerator(data);
-  //   const extent = [
-  //     0,
-  //     max(layers, (layer) => max(layer, (sequence) => sequence[1])),
-  //   ];
-
-  //   const xScale = scalePoint()
-  //     .domain(data.map((d) => d.date.slice(0, 10)))
-  //     .range([0, width]);
-
-  //   const yScale = scaleLinear().domain(extent).range([height, 0]);
-
-  //   const areaGenerator = area()
-  //     .x((sequence) => xScale(sequence.data.date.slice(0, 10)))
-  //     .y0((sequence) => yScale(sequence[0]))
-  //     .y1((sequence) => yScale(sequence[1]))
-  //     .curve(curveCardinal);
-
-  //   svg
-  //     .selectAll('.layer')
-  //     .data(layers)
-  //     .join('path')
-  //     .attr('class', 'layer')
-  //     .attr('fill', (layer) => colors[layer.key])
-  //     .attr('d', areaGenerator);
-
-  //   const xAxis = axisBottom(xScale);
-  //   svg
-  //     .select('.x-axis')
-  //     .attr('transform', `translate(0, ${height})`)
-  //     .call(xAxis);
-
-  //   const yAxis = axisLeft(yScale);
-  //   svg.select('.y-axis').call(yAxis);
-  // }, [colors, data, dimensions, keys]);
 
   return (
     <Layout>
@@ -85,12 +25,7 @@ const DailyEvents = ({ stats }) => {
 
       <StackedChart stats={stats} keys={keys} colors={colors} />
 
-      {/* <div ref={wrapperRef} style={{ marginBottom: '2rem' }}>
-        <svg ref={svgRef}>
-          <g className='x-axis' />
-          <g className='y-axis' />
-        </svg>
-      </div> */}
+      <h2 className={styles.title}>Database Tables</h2>
 
       <table className={styles.DailyEvents}>
         <thead>
--- client/src/pages/hourly-events.js
+++ client/src/pages/hourly-events.js
@@ -1,10 +1,96 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useState, useEffect, useRef } from 'react';
+import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout/Layout';
+import useResizeObserver from '../hooks/useResizeObserver';
+import {
+  select,
+  scaleBand,
+  axisBottom,
+  stack,
+  max,
+  scaleLinear,
+  axisLeft,
+  stackOrderAscending,
+} from 'd3';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const HourlyEvents = ({ stats }) => {
-  const [startDate, setStartDate] = useState(null);
+  const [startDate, setStartDate] = useState(new Date(stats[0].date));
+
+  let filteredStats = stats.filter((stat) => {
+    return stat.date === startDate.toISOString();
+  });
+
+  let dates = stats.map((stat) => new Date(stat.date).toISOString());
+
+  const changeDate = (date) => {
+    setStartDate(date);
+  };
+
+  const allKeys = ['events'];
+
+  const colors = {
+    events: 'darkturquoise',
+  };
+
+  const [keys, setKeys] = useState(allKeys);
+  const [data, setData] = useState(filteredStats);
+
+  useEffect(() => {
+    filteredStats = stats.filter((stat) => {
+      return stat.date === startDate.toISOString();
+    });
+    setData(filteredStats);
+  }, [startDate]);
+
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const { width, height } =
+      dimensions || wrapperRef.current.getBoundingClientRect();
+
+    const stackGenerator = stack().keys(keys).order(stackOrderAscending);
+    const layers = stackGenerator(data);
+    const extent = [
+      0,
+      max(layers, (layer) => max(layer, (sequence) => sequence[1])),
+    ];
+
+    const xScale = scaleBand()
+      .domain(data.map((d) => d.hour))
+      .range([0, width])
+      .padding(0.25);
+
+    const yScale = scaleLinear().domain(extent).range([height, 0]);
+
+    svg
+      .selectAll('.layer')
+      .data(layers)
+      .join('g')
+      .attr('class', 'layer')
+      .attr('fill', (layer) => colors[layer.key])
+      .selectAll('rect')
+      .data((layer) => layer)
+      .join('rect')
+      .attr('x', (sequence) => xScale(sequence.data.hour))
+      .attr('width', xScale.bandwidth())
+      .attr('y', (sequence) => yScale(sequence[1]))
+      .attr('height', (sequence) => yScale(sequence[0]) - yScale(sequence[1]));
+
+    const xAxis = axisBottom(xScale);
+    svg
+      .select('.x-axis')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis);
+
+    const yAxis = axisLeft(yScale);
+    svg.select('.y-axis').call(yAxis);
+  }, [colors, data, dimensions, keys]);
 
   return (
     <Layout>
@@ -14,6 +100,23 @@ const HourlyEvents = ({ stats }) => {
       </Head>
 
       <h1 className={styles.title}>Event Charts</h1>
+      <h2 className={styles.title}>Hourly Events</h2>
+
+      <div ref={wrapperRef} style={{ marginBottom: '2rem' }}>
+        <svg ref={svgRef}>
+          <g className='x-axis' />
+          <g className='y-axis' />
+        </svg>
+      </div>
+
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => changeDate(date)}
+        minDate={new Date(dates[0])}
+        maxDate={new Date(dates[dates.length - 1])}
+        placeholderText='Select a date!'
+      />
+
       <h2 className={styles.title}>Database Tables</h2>
 
       <table className={styles.HourlyEvents}>
