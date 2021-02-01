34aa0025e260e11367feba459f312d56bf38a617
Randy Shao <17rs63@queensu.ca>
2021-02-01 01:18:48 -0500

    stacked charted component

--- client/src/pages/daily-events.js
+++ client/src/pages/daily-events.js
@@ -2,19 +2,20 @@ import Head from 'next/head';
 import { useState, useEffect, useRef } from 'react';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout/Layout';
-import useResizeObserver from '../hooks/useResizeObserver';
-import {
-  select,
-  axisBottom,
-  stack,
-  max,
-  scaleLinear,
-  axisLeft,
-  stackOrderAscending,
-  area,
-  scalePoint,
-  curveCardinal,
-} from 'd3';
+import StackedChart from '../components/Chart/StackedChart';
+// import useResizeObserver from '../hooks/useResizeObserver';
+// import {
+//   select,
+//   axisBottom,
+//   stack,
+//   max,
+//   scaleLinear,
+//   axisLeft,
+//   stackOrderAscending,
+//   area,
+//   scalePoint,
+//   curveCardinal,
+// } from 'd3';
 
 const DailyEvents = ({ stats }) => {
   const allKeys = ['events'];
@@ -24,53 +25,53 @@ const DailyEvents = ({ stats }) => {
   };
 
   const [keys, setKeys] = useState(allKeys);
-  const [data, setData] = useState(stats);
+  // const [data, setData] = useState(stats);
 
-  const svgRef = useRef();
-  const wrapperRef = useRef();
-  const dimensions = useResizeObserver(wrapperRef);
+  // const svgRef = useRef();
+  // const wrapperRef = useRef();
+  // const dimensions = useResizeObserver(wrapperRef);
 
-  useEffect(() => {
-    const svg = select(svgRef.current);
-    const { width, height } =
-      dimensions || wrapperRef.current.getBoundingClientRect();
+  // useEffect(() => {
+  //   const svg = select(svgRef.current);
+  //   const { width, height } =
+  //     dimensions || wrapperRef.current.getBoundingClientRect();
 
-    const stackGenerator = stack().keys(keys).order(stackOrderAscending);
-    const layers = stackGenerator(data);
-    const extent = [
-      0,
-      max(layers, (layer) => max(layer, (sequence) => sequence[1])),
-    ];
+  //   const stackGenerator = stack().keys(keys).order(stackOrderAscending);
+  //   const layers = stackGenerator(data);
+  //   const extent = [
+  //     0,
+  //     max(layers, (layer) => max(layer, (sequence) => sequence[1])),
+  //   ];
 
-    const xScale = scalePoint()
-      .domain(data.map((d) => d.date.slice(0, 10)))
-      .range([0, width]);
+  //   const xScale = scalePoint()
+  //     .domain(data.map((d) => d.date.slice(0, 10)))
+  //     .range([0, width]);
 
-    const yScale = scaleLinear().domain(extent).range([height, 0]);
+  //   const yScale = scaleLinear().domain(extent).range([height, 0]);
 
-    const areaGenerator = area()
-      .x((sequence) => xScale(sequence.data.date.slice(0, 10)))
-      .y0((sequence) => yScale(sequence[0]))
-      .y1((sequence) => yScale(sequence[1]))
-      .curve(curveCardinal);
+  //   const areaGenerator = area()
+  //     .x((sequence) => xScale(sequence.data.date.slice(0, 10)))
+  //     .y0((sequence) => yScale(sequence[0]))
+  //     .y1((sequence) => yScale(sequence[1]))
+  //     .curve(curveCardinal);
 
-    svg
-      .selectAll('.layer')
-      .data(layers)
-      .join('path')
-      .attr('class', 'layer')
-      .attr('fill', (layer) => colors[layer.key])
-      .attr('d', areaGenerator);
+  //   svg
+  //     .selectAll('.layer')
+  //     .data(layers)
+  //     .join('path')
+  //     .attr('class', 'layer')
+  //     .attr('fill', (layer) => colors[layer.key])
+  //     .attr('d', areaGenerator);
 
-    const xAxis = axisBottom(xScale);
-    svg
-      .select('.x-axis')
-      .attr('transform', `translate(0, ${height})`)
-      .call(xAxis);
+  //   const xAxis = axisBottom(xScale);
+  //   svg
+  //     .select('.x-axis')
+  //     .attr('transform', `translate(0, ${height})`)
+  //     .call(xAxis);
 
-    const yAxis = axisLeft(yScale);
-    svg.select('.y-axis').call(yAxis);
-  }, [colors, data, dimensions, keys]);
+  //   const yAxis = axisLeft(yScale);
+  //   svg.select('.y-axis').call(yAxis);
+  // }, [colors, data, dimensions, keys]);
 
   return (
     <Layout>
@@ -82,12 +83,14 @@ const DailyEvents = ({ stats }) => {
       <h1 className={styles.title}>Event Charts</h1>
       <h2 className={styles.title}># of Events</h2>
 
-      <div ref={wrapperRef} style={{ marginBottom: '2rem' }}>
+      <StackedChart stats={stats} keys={keys} colors={colors} />
+
+      {/* <div ref={wrapperRef} style={{ marginBottom: '2rem' }}>
         <svg ref={svgRef}>
           <g className='x-axis' />
           <g className='y-axis' />
         </svg>
-      </div>
+      </div> */}
 
       <table className={styles.DailyEvents}>
         <thead>
